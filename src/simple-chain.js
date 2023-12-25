b3a6c57bdfd3bedba39d7a492e7af84fdc0b79c9
Raman Martsulevich <dwarf.md@gmail.com>
2023-12-25 13:11:25 +0100

    feat: make chain finished

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+   arr : [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  addLink(value) {
+    if(typeof value === 'undefined'){
+      this.arr.push(' ')
+    }
+    else if (value === null){
+        console.log("jj");
+       this.arr.push(String(value));
+    }
+    else
+      this.arr.push(value.toString());
+      
+   return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  removeLink(position) {
+   // console.log("pos", position);
+    if(!Number.isInteger(position) || position > this.arr.length - 1 || position < 1){
+      this.arr = [];
+      throw Error('You can\'t remove incorrect link!')
+    }
+    else{
+       this.arr.splice(position-1, 1)
+       return this
+    }
+     
   },
+
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   this.arr = this.arr.reverse();
+   return this
   },
+
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = '';
+    this.arr.forEach((element, index)=>{
+      let buf = `( ${element} )`
+      if(index != 0){
+        buf = `~~${buf}`
+      }
+      str = str.concat(buf);
+    })
+    this.arr = [];
+    return str;
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+// console.log(chainMaker.addLink('a').addLink('b').addLink('c').addLink().removeLink(1).finishChain()); ;
+// console.log(chainMaker.arr);
+// console.log(chainMaker.getLength());
+// console.log(Number.isInteger(7));
+// console.log(Number.isInteger(7.1));
+//console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2));
+
+
+// () => chainMaker.addLink(1).addLink(2).addLink(3).removeLink(0),
+// () => chainMaker.addLink(1).addLink(2).addLink(3).removeLink('2nd'),
+// () => chainMaker.addLink(1).addLink(2).addLink(3).removeLink(-2),
+// () => chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4)
+
+//console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain())
+//'( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )');
+
+//console.log(chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain())
+
+
+//'( NaN )~~( null )~~( true )~~( false )~~( 1.233 )')
--- src/transform-array.js
+++ src/transform-array.js
@@ -9,13 +9,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * @example
  * 
+ * --discard-next excludes the next element of the array from the transformed array.
+  --discard-prev excludes the previous element of the array from the transformed array.
+  --double-next duplicates the next element of the array in the transformed array.
+  --double-prev duplicates the previous element of the array in the transformed array.
+ * 
+ * 
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw Error("'arr' parameter must be an instance of the Array!")
+  }
+  let arrCopy = [...arr];
+  let bufArr = [];
+
+  for (let index = 0; index < arr.length; index++) {
+  
+    const element = arr[index];
+    if(element === '--double-next' ){
+      if(arrCopy[index + 1] != null){
+        arrCopy[index] = arrCopy[index + 1];
+      }
+    }
+    if(element === '--double-prev' ){
+      if(arrCopy[index - 1] != null){
+        arrCopy[index] = arrCopy[index - 1];
+      }
+    }
+  if(element === '--discard-next'){
+    if(index != arrCopy.length - 1){
+      delete arrCopy[index + 1]
+    }
+    else {
+      arrCopy.pop()
+    }
+    
+  }
+  if(element === '--discard-prev'){
+    if(index != 0){
+      delete arrCopy[index - 1]
+    }
+   
+  }
+    
+  }
+
+  arrCopy.forEach((element, index ) => {
+    if(element === '--discard-prev' || 
+       element === '--discard-next' ||
+        element === '--double-next' ||
+        element === '--double-prev'
+       ){
+        delete arrCopy[index]
+    }
+
+    })
+  //console.log(arrCopy);
+  bufArr = arrCopy.filter(element => {
+    return element != null
+  })
+  
+  return bufArr
 }
 
 module.exports = {
